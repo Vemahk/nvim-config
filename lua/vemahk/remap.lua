19ffb2429c843ff93bdc6e078478e7a911f70f67
Zachary Vemahk <vemahk20@gmail.com>
2023-10-10 15:00:05 -0500

    search stay in middle

--- lua/vemahk/remap.lua
+++ lua/vemahk/remap.lua
@@ -10,6 +10,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
