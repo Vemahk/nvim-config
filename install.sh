a2ca9f34c2bd14a514a9d31bb32fcc28bf443cc3
Vemahk <vemahk20@gmail.com>
2024-02-19 02:09:13 -0600

    more install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+npm i -g vscode-langservers-extracted
