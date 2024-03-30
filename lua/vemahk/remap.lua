7ab4b3dd373f142e863f21d7fdc56d682db02026
Vemahk <vemahk20@gmail.com>
2024-03-29 21:47:09 -0500

    copilot rebinds

--- lua/vemahk/remap.lua
+++ lua/vemahk/remap.lua
@@ -19,15 +19,22 @@ map("n", "N", "Nzzzv")
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<M-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<M-.>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<M-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<M->>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<M-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', function ()
+map('n', '<M-c>', function ()
 	vim.cmd [[BufferClose]]
 	vim.cmd.Ex()
 end, opts)
+
+map('i', '<C-y>', 'copilot#Accept("")', {
+    expr = true,
+    replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
+
